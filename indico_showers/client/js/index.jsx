b2fc5d1f9a89c7752669368d06ea21302a427f28
Pedro Ferreira <pedro.ferreira@cern.ch>
2019-03-26 18:33:38 +0100

    Add splash image and fix wording 💖

--- indico_showers/client/js/index.jsx
+++ indico_showers/client/js/index.jsx
@@ -8,27 +8,63 @@
 
 import setup from 'indico/modules/rb_new/setup';
 import DefaultApp from 'indico/modules/rb_new/components/App';
+import DefaultBookRoom from 'indico/modules/rb_new/modules/bookRoom/BookRoom';
+import DefaultBookRoomModal from 'indico/modules/rb_new/modules/bookRoom/BookRoomModal';
+import DefaultLandingStatistics from 'indico/modules/rb_new/modules/landing/LandingStatistics';
 import DefaultMenu from 'indico/modules/rb_new/components/Menu';
+import DefaultRoomDetailsModal from 'indico/modules/rb_new/common/rooms/RoomDetailsModal';
 
 import {Translate} from 'indico/react/i18n';
 import {parametrize} from 'indico/react/util';
 
 
-const ShowersApp = parametrize(DefaultApp, {
+const App = parametrize(DefaultApp, {
     title: Translate.string('Showers'),
     iconName: 'shower'
 });
 
-const ShowersMenu = parametrize(DefaultMenu, () => ({
+const Menu = parametrize(DefaultMenu, () => ({
     labels: {
         bookRoom: Translate.string('Reserve a shower'),
         roomList: Translate.string('List of Showers')
     }
 }));
 
+const BookRoom = parametrize(DefaultBookRoom, {
+    showSuggestions: false,
+    labels: {
+        bookButton: Translate.string('Book Shower'),
+        preBookButton: Translate.string('Pre-Book Shower'),
+        detailsButton: Translate.string('See details')
+    }
+});
+
+const LandingStatistics = parametrize(DefaultLandingStatistics, () => ({
+    labels: {
+        activeRooms: Translate.string('Showers available'),
+        bookingsToday: Translate.string('Refreshing Showers Today')
+    }
+}));
+
+const RoomDetailsModal = parametrize(DefaultRoomDetailsModal, () => ({
+    title: Translate.string('Shower Details')
+}));
+
+const BookRoomModal = parametrize(DefaultBookRoomModal, () => ({
+    defaultTitles: {
+        booking: Translate.string('Book a Shower'),
+        preBooking: Translate.string('Pre-book a Shower')
+    },
+    reasonRequired: false,
+}));
+
 const parametrized = {
-    App: ShowersApp,
-    Menu: ShowersMenu,
+    App,
+    Menu,
+    BookRoom,
+    LandingStatistics,
+    RoomDetailsModal,
+    BookRoomModal
 };
 
 setup({...parametrized});
